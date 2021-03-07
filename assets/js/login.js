fe96db0cacebdb91e341b579ba3ecd870c762b04
ckj <2313364746@qq.com>
2021-03-07 22:38:22 +0800

    完成了登录注册功能开发

---
+++ assets/js/login.js
@@ -0,0 +1,60 @@
+$(function () {
+  //去注册账号
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide();
+    $('.reg-box').show();
+  })
+  //去登陆
+  $('#link_login').on('click', function () {
+    $('.login-box').show();
+    $('.reg-box').hide();
+  })
+  //步骤一：从layui中获取form对象
+  var form = layui.form;
+  var layer = layui.layer;
+  var data = {
+    username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val()
+  };
+  form.verify({
+    pass: [
+      /^[\S]{6,12}$/,
+      '密码必须6到12位，且不能出现空格'
+    ],
+    repass: function (value) {
+      var pwd = $('.reg-box [name=password]').val();
+      if (pwd !== value) {
+        return '两次密码输入不一致';
+      }
+    }
+  })
+
+  //注册功能
+  $('#form-reg').on('submit', function (e) {
+    e.preventDefault();
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message);
+      }
+      layer.msg(res.message);
+      $('#link_login').click();
+    })
+  })
+  $('#form-login').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/api/login',
+      method: 'post',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
+        layer.msg('登录成功！');
+        // console.log(res.token);
+        // 将登录成功得到的token值存储到本地
+        localStorage.setItem('token', res.token);
+        location.href = '/index.html';
+      }
+    })
+  })
+})
